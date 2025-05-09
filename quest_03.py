43dffa81fe48e220f2629f3469d3a4887893401c
Franco Ezequiel <eze.cavs@hotmail.com>
2025-05-09 20:37:52 -0300

    UPDATE: Made the challenge of the Matrices

--- quest_03.py
+++ quest_03.py
@@ -50,14 +50,12 @@ def validar_mismo_tamanio_matriz(matriz_a: list[list], matriz_b: list[list]) -> 
     columnas_a = 0
     columnas_b = 0
 
-    print(f"filas_a: {filas_a}")
-    print(f"filas_b: {filas_b}")
     #Recorremos la cantidad de columnas de cada matriz
     for index in range(filas_a):
         columnas_a += len(matriz_a[index])
 
     for column in range(len(matriz_b[index])):
-        columnas_b += len(matriz_b[index])
+        columnas_b += len(matriz_b[column])
     #Dividimos la cantidad de columnas por la cantidad de filas
     cantidad_columnas_a = columnas_a / filas_a
     cantidad_columnas_b = columnas_b / filas_b
@@ -72,25 +70,145 @@ def validar_mismo_tamanio_matriz(matriz_a: list[list], matriz_b: list[list]) -> 
     #Creamos los valores para ver que coincidan filas y columnas
     igualdad_filas = filas_a == filas_b
     igualdad_columnas = cantidad_columnas_a == cantidad_columnas_b
-
     validacion = igualdad_filas & igualdad_columnas
 
-    print(f"igualdad_filas: {igualdad_filas}")
-    print(f"igualdad_columnas: {igualdad_columnas}")
-    print(f"Validación: {validacion}")
     return validacion
+
+def validar_multiplicacion_matriz(matriz_a: list[list], matriz_b: list[list]) -> bool:
+    """
+    Parámetros: Recibe 2 matrices
     
+    ¿Qué hace?: Valida que la cantidad de columnas matriz A 
+    sea igual que la cantidad de filas de la columna B
+
+    ¿Qué Retorna?:Retorna un booleano con true 
+    en caso de que sea válida la multiplicación, o false en caso contrario.
+    """
+    #Definimos las variables de filas y columnas
+    columnas_a = 0
+    filas_a = len(matriz_a)
+    filas_b = len(matriz_b)
+
+    #Recorremos la cantidad de columnas de la matriz A
+    for index in range(len(matriz_a)):
+        columnas_a += len(matriz_a[index])
+    
+    #Dividimos la cantidad de columnas por la cantidad de filas
+    cantidad_columnas_a = columnas_a / filas_a
+
+    #Creamos los valores para ver que coincidan filas y columnas
+    validacion =  cantidad_columnas_a == filas_b
+    print(f"validacion: {validacion}")
+    return validacion
+
 
 def suma_matrices(matriz_a: list[list], matriz_b: list[list]) -> list[list]:
+    """
+    Parámetros: Recibe 2 matrices
+    
+    ¿Qué hace?: Primero valida que se pueda realizar la suma entre matrices.
+    Acto seguido inicializa una matriz con la cantidad de filas y columnas.
+    Para posteriormente recorrer una matriz, para ir sumando los valores entre 
+    ambas matrices, y guardar la suma en esta matriz nueva.
+
+    ¿Qué Retorna?:Retorna una matriz que contiene la suma de ambas matrices.
+    """
     matriz_resultante = []
     if validar_mismo_tamanio_matriz(matriz_a, matriz_b):
-        pass
+        matriz_resultante = inicializar_matriz(len(matriz_a), len(matriz_a[0]), None)
+        print(f"Suma de Matrices:\n")
+
+        for indice_fila in range(len(matriz_a)):
+            for indice_columna in range(len(matriz_a[indice_fila])):
+                resultado = matriz_a[indice_fila][indice_columna] + matriz_b[indice_fila][indice_columna]
+                matriz_resultante[indice_fila][indice_columna] = resultado
+                print(matriz_resultante[indice_fila][indice_columna], end=" ")
+            print("")
+
     else:
         print("ERROR: Las matrices no tienen el mismo tamaño.")
+    
     return matriz_resultante
 
+def multiplicar_matrices_por_escalar(matriz_a: list[list], multiplo: int) -> list[list]:
+    """
+    Parámetros: Recibe 2 matrices
+    
+    ¿Qué hace?: Primero valida que se pueda realizar la suma entre matrices.
+    Acto seguido inicializa una matriz con la cantidad de filas y columnas.
+    Para posteriormente recorrer una matriz, para ir sumando los valores entre 
+    ambas matrices, y guardar la suma en esta matriz nueva.
+
+    ¿Qué Retorna?:Retorna una matriz que contiene la suma de ambas matrices.
+    """
+
+    matriz_resultante = inicializar_matriz(len(matriz_a), len(matriz_a[0]), None)
+    print(f"Suma de Matrices:\n")
+
+    for indice_fila in range(len(matriz_a)):
+        for indice_columna in range(len(matriz_a[indice_fila])):
+            resultado = matriz_a[indice_fila][indice_columna] * multiplo
+            matriz_resultante[indice_fila][indice_columna] = resultado
+            print(matriz_resultante[indice_fila][indice_columna], end=" ")
+        print("")
+
+    return matriz_resultante
+
+def multiplicar_matrices(matriz_a: list[list], matriz_b: list[list]) -> list[list]:
+    """
+    Parámetros: Recibe 2 matrices
+    
+    ¿Qué hace?: Primero valida que se pueda realizar la suma entre matrices.
+    Acto seguido inicializa una matriz con la cantidad de filas y columnas.
+    Para posteriormente recorrer una matriz, para ir sumando los valores entre 
+    ambas matrices, y guardar la suma en esta matriz nueva.
+
+    ¿Qué Retorna?:Retorna una matriz que contiene el resultado 
+    de la multiplicación  de ambas matrices.
+    """
+
+    if validar_multiplicacion_matriz(matriz_a, matriz_b):
+
+        matriz_resultante = inicializar_matriz(len(matriz_a), len(matriz_b[0]), None)
+        print(f"Multiplica las Matrices:\n")
+
+        #Recorremos la cantidad de veces según las filas de la matriz A
+        for indice_fila in range(len(matriz_a)):
+            #Dentro, definimos el rango de la cantidad de Columnas de la matriz B
+            for columnas_b in range(len(matriz_b[0])):
+                #Definimos un valor inicial para guardar el elemento
+                valor_a_guardar = 0
+                valor_indice = 0
+                #Recorremos las FILAS de la matriz B, la cantidad de veces según las columnas (ej: 4)
+                for index_matriz_b in range(len(matriz_b)):
+                    #Multiplicamos las matrices
+                    resultado_multiplicacion = matriz_a[indice_fila][index_matriz_b] * matriz_b[index_matriz_b][columnas_b]
+                    #Una vez obtenida la multiplicación, lo asignamos a "valor_indice"
+                    #Para que quede el histórico de cuentas y multiplicaciones de este elemento 
+                    valor_indice += resultado_multiplicacion
+
+                #Asignamos el valor_indice final, ya con la suma de las multiplicaciones
+                valor_a_guardar = valor_indice
+                #Y lo asignamos en la posición correspondiente de esta nueva matriz
+                matriz_resultante[indice_fila][columnas_b] = valor_a_guardar
+
+    else:
+        print("ERROR: No se puede multiplicar las matrices.")
+
+    print(f"matriz_resultante: {matriz_resultante}")
+    return matriz_resultante
 #cargar_matriz_aleatoriamente(matriz_a)
 
-suma_matrices(matriz_a, matriz_b)
+matriz_1 = [
+    [2,3],
+    [-2,9],
+    [6,-4],
+]
+matriz_2 = [
+    [-2,8,5,3],
+    [5,-6,7,-7],
 
-print(matriz_a)
+]
+#suma_matrices(matriz_1, matriz_2)
+#multiplicar_matrices_por_escalar(matriz_1, 3)
+multiplicar_matrices(matriz_1, matriz_2)
